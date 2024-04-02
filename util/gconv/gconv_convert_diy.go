5d1206ccab9fb5cec501a5acc46ea233cfbd4015
ShuQingZai <overbeck.jack@idd.cool>
2024-04-02 14:59:09 +0800

    [gconv] gconv 转换支持 gorm 的 JSON 类型

--- util/gconv/gconv_convert_diy.go
+++ util/gconv/gconv_convert_diy.go
@@ -1,6 +1,7 @@
 package gconv
 
 import (
+	"encoding/json"
 	"fmt"
 )
 
@@ -9,10 +10,41 @@ func iddConvertDiy(in doConvertInput) (any, error) {
 	switch in.ToTypeName {
 	// gorm.io/datatypes.JSON
 	case "datatypes.JSON":
-		return Bytes(in.FromValue), nil
+		return iddToDatatypesJSON(in.FromValue), nil
 	// gorm.io/datatypes.JSONMap
 	case "datatypes.JSONMap":
 		return Map(in.FromValue), nil
 	}
 	return nil, fmt.Errorf("unsupported cast type: %T To %s", in.FromValue, in.ToTypeName)
+}
+
+// iddToDatatypesJSON idd convert v to datatypes.JSON
+//
+// See: Bytes
+func iddToDatatypesJSON(v any) []byte {
+	if v == nil {
+		return nil
+	}
+	switch vv := v.(type) {
+	case string:
+		return []byte(vv)
+	case *string:
+		return []byte(*vv)
+	case []byte:
+		return vv
+	case *[]byte:
+		return *vv
+	case json.RawMessage:
+		return vv
+	case *json.RawMessage:
+		return *vv
+	default:
+		if f, ok := vv.(iBytes); ok {
+			return f.Bytes()
+		}
+		if b, err := json.Marshal(v); err == nil {
+			return b
+		}
+		return nil
+	}
 }
